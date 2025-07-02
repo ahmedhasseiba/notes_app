40638ed0d0969216a19bec6ddf0595c662eb5b23
ahmed hasseiba <hasseiba.hasseiba90@gmail.com>
2025-07-02 09:33:18 +0300

    create_custom_note_item

---
+++ lib/views/widgets/custom_icon_search.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomIconSearch extends StatelessWidget {
+  const CustomIconSearch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      width: 46,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(.05),
+      ),
+
+      child: Center(child: Icon(Icons.search, size: 24)),
+    );
+  }
+}
---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 16, bottom: 16, left: 16),
+      decoration: BoxDecoration(
+        color: Color(0xffffd97e),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 28),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16.0, bottom: 16),
+              child: Text(
+                'build your career with m ahmed',
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 33, 31, 31),
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, color: Colors.black, size: 30),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Text('may 21 , 2022', style: TextStyle(color: Colors.black)),
+          ),
+        ],
+      ),
+    );
+  }
+}
