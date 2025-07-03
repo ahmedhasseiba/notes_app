216f3f89f6f1db56c2b6f33d4b5c28cf71adace3
ahmed hasseiba <hasseiba.hasseiba90@gmail.com>
2025-07-03 06:26:31 +0300

    add fiel to note buttom sheet

--- lib/views/widgets/add_note_buttom_sheet.dart
+++ lib/views/widgets/add_note_buttom_sheet.dart
@@ -7,8 +7,15 @@ class AddNoteButtomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(children: [SizedBox(height: 32), CustomTextField()]),
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          SizedBox(height: 32),
+          CustomTextField(hint: 'title'),
+          SizedBox(height: 16),
+          CustomTextField(hint: 'content', maxLines: 5),
+        ],
+      ),
     );
   }
 }
